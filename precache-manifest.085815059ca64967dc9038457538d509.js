78dfb30d57b519ac980042de3353f1a88f671f5d
Nithin Gunasekaran <nithin.ktg@gmail.com>
2019-04-04 19:32:15 +0530

    Updates

---
+++ precache-manifest.085815059ca64967dc9038457538d509.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4464e7446c55de0ba537",
+    "url": "/milestones/static/css/main.7b27b04f.chunk.css"
+  },
+  {
+    "revision": "4464e7446c55de0ba537",
+    "url": "/milestones/static/js/main.357aba04.chunk.js"
+  },
+  {
+    "revision": "0df304bea9aa6ca4d6f2",
+    "url": "/milestones/static/js/runtime~main.1091f90a.js"
+  },
+  {
+    "revision": "a60b89e173d91bd70c80",
+    "url": "/milestones/static/js/2.60a7fb5d.chunk.js"
+  },
+  {
+    "revision": "67fb4ef55553776931a9fd671dc172eb",
+    "url": "/milestones/index.html"
+  }
+];
